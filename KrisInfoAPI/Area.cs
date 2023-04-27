7799b81de194352318472d7b3784fe71afa2c993
Christoffer Sladin <christoffer.sladin@hotmail.com>
2023-04-27 09:37:07 +0200

    added properties to VMs

---
+++ KrisInfoAPI/Area.cs
@@ -0,0 +1,6 @@
+﻿namespace KrisInfoAPI
+{
+    public class Area
+    {
+    }
+}
--- KrisInfoAPI/KrisInfoVM.cs
+++ KrisInfoAPI/KrisInfoVM.cs
@@ -2,5 +2,9 @@
 {
     public class KrisInfoVM
     {
+        public int Identifier { get; set; }
+        public string PushMessage { get; set; }
+        public DateTime Published { get; set; }
+        public List<Area> Area { get; set; }
     }
 }
